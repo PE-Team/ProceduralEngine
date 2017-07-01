04c79b526807fc996c0bcfba61a762919095668a
FireEmber <empenn54@gmail.com>
2017-06-30 19:03:09 -0500

    Started work on FontRenderer

--- src/pe/engine/graphics/objects/FontRenderer.java
+++ src/pe/engine/graphics/objects/FontRenderer.java
@@ -2,4 +2,28 @@ package pe.engine.graphics.objects;
 
 public class FontRenderer implements RenderableI, BatchRenderableI{
 
+	@Override
+	public void batchStart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void batchRender() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void batchStop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
